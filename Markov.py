50e937e260569b005b2d1b3f8e77923e0ca0354d
yash98 <yashmalviya98@gmail.com>
2018-10-23 14:49:44 +0530

    some more markov

--- Markov.py
+++ Markov.py
@@ -2,7 +2,7 @@
 
 
 class Transition:
-    
+
     def init(self, probability, action, state_rep):
         # has params
         # probability: float
@@ -13,10 +13,11 @@ class Transition:
         self.action = action
         self.state_rep = state_rep
 
-
-    def make_transitions(self, rep_first, rep_second, dealers_first_car, fcp):
+    def make_transitions(self, rep_first, rep_second, dealers_first_card, fcp):
+        # TODO: Assumed fcp is float
+        trans_list_to_return = []
         # Consider all action possiblities
-        
+   
         # ACTION HIT
 
         # loop over all card possiblities
@@ -26,7 +27,8 @@ class Transition:
             if rep_first == 0:
                 # ace comes for a hand containing no ace previously
                 if card_possiblity == 1:
-                    if rep_second <= 
+                    if rep_second < 10:
+                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', (1, rep_second, dealers_first_card)))
                 else if 2 <= card_possiblity <= 9:
                 else:
 
@@ -39,13 +41,18 @@ class Transition:
 
 
 class State:
-    def init(self, rep_first, rep_second, dealers_first_car, face_card_probability):
+    def init(self, rep_first, rep_second, dealers_first_card, is_goal, face_card_probability):
         self.rep_first = rep_first
         self.rep_second = rep_second
-        self.dealers_first_car = dealers_first_car
+        self.dealers_first_card = dealers_first_card
+        
+        self.is_goal = is_goal
         # TODO: correct to make goal states bust and states after stop.
         # the states that inherent reward.
-        self.transitions = make_transitions(rep_first, rep_second, dealers_first_car, face_card_probability)
+        self.transitions = []
+        # absorbing goals can't get out of goal
+        if !(is_goal):
+            self.transitions = make_transitions(rep_first, rep_second, dealers_first_card, face_card_probability)
 
 # initialize all states for the game here.
 class Markov:
