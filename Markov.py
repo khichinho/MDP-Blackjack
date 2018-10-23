ed7ace8eaba9d368e670ed2dfe3516e9e1a3c60e
yash98 <yashmalviya98@gmail.com>
2018-10-23 18:32:21 +0530

    handle hit transitions of hard value and some of soft

--- Markov.py
+++ Markov.py
@@ -7,7 +7,7 @@ class Transition:
         # has params
         # probability: float
         # action: char
-        # state_rep: three tuple of int
+        # state_rep: four tuple of 3 int and 1 bool
 
         self.probability = probability
         self.action = action
@@ -27,25 +27,56 @@ class Transition:
             if rep_first == 0:
                 # ace comes for a hand containing no ace previously
                 if card_possiblity == 1:
-                    if rep_second <= 10:
-                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', (1, rep_second, dealers_first_card)))
+                    if rep_second <= 9:
+                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                        (1, rep_second, dealers_first_card, False)))
                     else:
-                        # bust
-                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', (0, rep_second+1, dealers_first_card)))
+                        # consider that ace = 1.
+                        if rep_second <= 20:
+                            trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                            (0, rep_second+1, dealers_first_card, False)))
+                        else:
+                            trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                            (21, 0, 0, False)))
                 # any non face non ace card
                 elif 2 <= card_possiblity <= 9:
                     if rep_second+card_possiblity > 21:
-                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', (21, 0, 0)))
+                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                        (21, 0, 0, False)))
                     else:
-                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', (0, rep_second+card_possiblity, dealers_first_card)))
+                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                        (0, rep_second+card_possiblity, dealers_first_card, False)))
                 # face card
                 else:
                     if rep_second+card_possiblity > 21:
-                        trans_list_to_return.append(Transition(fcp, 'H', (21, 0, 0)))
+                        trans_list_to_return.append(Transition(fcp, 'H', 
+                        (21, 0, 0, False)))
                     else:
-                        trans_list_to_return.append(Transition(fcp, 'H', (0, rep_second+card_possiblity, dealers_first_card)))
+                        trans_list_to_return.append(Transition(fcp, 'H', 
+                        (0, rep_second+card_possiblity, dealers_first_card, False)))
+           
             # soft values, Ace + something
             elif rep_first == 1 & rep_second != 1:
+                
+                # ace comes for a hand containing exactly 1 ace previously
+                if card_possiblity == 1:
+                    # consider that ace = 1.
+                    if rep_second <= 8:
+                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                        (1, rep_second+1, dealers_first_card, False)))
+                    else:
+                        # only rep_second is from 2 to 9 where rep_first is 1. 
+                        # So this only handle rep_second == 9.
+                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                        (11, 21, dealers_first_card, False)))
+               
+                # any non face non ace card
+                elif 2 <= card_possiblity <= 9:
+                    if rep_second+card_possiblity <= 9:
+                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                        (1, rep_second+card_possiblity, dealers_first_card, False)))
+                    else:
+                        
 
             #duplicates
             else:
