be047d49de407175f96894dce89ac00aae2ed619
yash98 <yashmalviya98@gmail.com>
2018-10-24 14:33:02 +0530

    Stand transition conditions done

--- Markov.py
+++ Markov.py
@@ -23,7 +23,7 @@ class Transition:
         # loop over all card possiblities
         for card_possiblity in xrange(1, 11):
 
-            # hard values
+            # HARD VALUES
             if rep_first == 0:
                 # ace comes for a hand containing no ace previously
                 if card_possiblity == 1:
@@ -55,11 +55,8 @@ class Transition:
                         trans_list_to_return.append(Transition(fcp, 'H', 
                         (0, rep_second+card_possiblity, dealers_first_card, False)))
            
-            # soft values, Ace + something
+            # SOFT VALUES, Ace + something
             elif rep_first == 1 & rep_second != 1:
-<<<<<<< HEAD
-                pass
-=======
                 
                 # ace comes for a hand containing exactly 1 ace previously
                 if card_possiblity == 1:
@@ -75,16 +72,101 @@ class Transition:
                
                 # any non face non ace card
                 elif 2 <= card_possiblity <= 9:
+                    # ace soft value states only go till 9
                     if rep_second+card_possiblity <= 9:
                         trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
                         (1, rep_second+card_possiblity, dealers_first_card, False)))
                     else:
-                        
+                        # consider ace as 1 not bust a rational player wouldn't bust hiimself. 
+                        # picking 11 as ace value
+                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                        (0, rep_second+card_possiblity+1, dealers_first_card, False)))
+                
+                # face card
+                else:
+                    # ace soft value states only go till 9
+                    if rep_second+card_possiblity <= 9:
+                        trans_list_to_return.append(Transition(fcp, 'H', 
+                        (1, rep_second+card_possiblity, dealers_first_card, False)))
+                    else:
+                        # consider ace as 1 not bust a rational player wouldn't bust hiimself. 
+                        # picking 11 as ace value
+                        trans_list_to_return.append(Transition(fcp, 'H', 
+                        (0, rep_second+card_possiblity+1, dealers_first_card, False)))
 
->>>>>>> ed7ace8eaba9d368e670ed2dfe3516e9e1a3c60e
-            #duplicates
+            # DUPLICATES
             else:
-                pass
+                if card_possiblity == 1:
+                    # aces after any duplicates
+                    trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                    (1, rep_second+rep_first, dealers_first_card, False)))
+                
+                elif 2 <= card_possiblity <= 9:
+                    # its duplicate aces
+                    if rep_first == 1:
+                        trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                        (1, rep_second+card_possiblity, dealers_first_card, False)))
+                    # any other duplicates
+                    else:
+                        if rep_first+rep_second+card_possiblity <= 21:
+                            trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                            (0, rep_first+rep_second+card_possiblity, dealers_first_card, False)))
+                        
+                        #bust
+                        else:
+                            trans_list_to_return.append(Transition((1-fcp)/9, 'H', 
+                            (21, 0, 0, False)))
+                
+                # face card
+                else:
+                    if rep_first+rep_second+card_possiblity <= 21:
+                        trans_list_to_return.append(Transition(fcp, 'H', 
+                        (0, rep_first+rep_second+card_possiblity, dealers_first_card, False)))
+                    
+                    #bust
+                    else:
+                        trans_list_to_return.append(Transition(fcp, 'H', 
+                        (21, 0, 0, False)))
+
+        # ACTION STAND
+
+        # when players stands, calculate the value of hand and chain to goal state 
+        if rep_first == 1:
+            # rational agent would want to bust itself and get highest value possible.
+            if rep_second == 1:
+                # option 1 consider both aces 1. value = 2
+                # option 2 consider both 11. value = 22. bust
+                # option 3 consider 1 and 11. value = 12. rational agent will choose higher for more chance of win
+                trans_list_to_return.append(Transition(1, 'S', 
+                (11, 12, dealers_first_card, False)))
+            
+            elif rep_second + 11 > 21:
+                trans_list_to_return.append(Transition(1, 'S', 
+                (11, rep_second+1, dealers_first_card, False)))
+            
+            # rep_sceond >= 1. case where ace+something ace being 11 bust already considered.
+            # what is only left is ace = 11 doesn't bust
+            else:
+                trans_list_to_return.append(Transition(1, 'S', 
+                (11, rep_second+11, dealers_first_card, False)))
+            
+        elif rep_first == 0:
+            # busting should be handle in hit case. here rep_first+rep_second <= 21 always.
+            trans_list_to_return.append(Transition(1, 'S', 
+            (11, rep_first+rep_second, dealers_first_card, False)))
+
+        # could even write else here because other states will most probably not violate these condition. just a precaution
+        elif 2 <= rep_first <= 10:
+            if 2 <= rep_second <= 10:
+                trans_list_to_return.append(Transition(1, 'S', 
+                (11, rep_first+rep_second, dealers_first_card, False)))
+            # no other case possible for this representation of states
+        
+        
+
+                
+
+
 class State:
     def init(self, rep_first, rep_second, dealers_first_card, is_first, is_goal, face_card_probability):
         self.rep_first = rep_first
