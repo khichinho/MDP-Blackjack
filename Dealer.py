5dbde10f778e99da4d726a0f0c48ba232fe8dfe8
Hardik Khichi <hardikkhichi@gmail.com>
2018-10-25 14:15:41 +0530

    dealer_dp_perfected

--- Dealer.py
+++ Dealer.py
@@ -5,10 +5,11 @@
 # function for returning reward of player after stand state.
 # called in Player Black Jack MDP
 
-
 #TODO: Handle doubling on player side
 
 #TODO: Input probability
+
+reward_dict = {}
 
 def rewardPlayer(player_hand_value, dealer_first_card, probability_face_card):
 	if(dealer_first_card == 1):
@@ -20,36 +21,48 @@ def rewardPlayer(player_hand_value, dealer_first_card, probability_face_card):
 #else !1
 def reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value, probability_face_card):
 
-	expected_reward = 0
+	r = 0
+	config_key = (player_hand_value, is_ace_card, dealer_hand_value)
 
-	if(dealer_hand_value > 21):
-		if(is_ace_card == 1):
-			return(reward_player_calculate(player_hand_value,0,dealer_hand_value-10, probability_face_card))
-		else:
-			return(tellReward(player_hand_value, dealer_hand_value))
-	elif(dealer_hand_value >= 17):
-		return(tellReward(player_hand_value, dealer_hand_value))
+	if config_key in reward_dict:
+		return reward_dict[config_key]
 	else:
-		for i in range(1,11):
-			if(i == 10):
-				expected_reward += probability_face_card * reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value+10, probability_face_card)
-			elif(i == 1):
-				expected_reward += ((1-probability_face_card)/9) * reward_player_calculate(player_hand_value, 1, dealer_hand_value+11, probability_face_card)
+		if(dealer_hand_value > 21):
+			if(is_ace_card == 1):
+				return(reward_player_calculate(player_hand_value,0,dealer_hand_value-10, probability_face_card))
 			else:
-				expected_reward += ((1-probability_face_card)/9) * reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value+i, probability_face_card)
-	return(expected_reward)
+				r = tellReward(player_hand_value, is_ace_card, dealer_hand_value)
+				reward_dict[config_key] = r
+		elif(dealer_hand_value >= 17):
+			r = tellReward(player_hand_value, is_ace_card, dealer_hand_value)
+			reward_dict[config_key] = r
+		else:
+			for i in range(1,11):
+				if(i == 10):
+					r += probability_face_card * reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value+10, probability_face_card)
+					reward_dict[config_key] = r
+				elif(i == 1):
+					r += ((1-probability_face_card)/9) * reward_player_calculate(player_hand_value, 1, dealer_hand_value+11, probability_face_card)
+					reward_dict[config_key] = r
+				else:
+					r += ((1-probability_face_card)/9) * reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value+i, probability_face_card)
+					reward_dict[config_key] = r
+		return(r)
 
-def tellReward(player_hand_value, dealer_hand_value):
+def tellReward(player_hand_value, is_ace_card, dealer_hand_value):
 	if(dealer_hand_value > 21):
 		if(player_hand_value == 21):
 			return 1.5
 		elif(player_hand_value < 21):
 			return 1
 	elif(dealer_hand_value == 21):
-		if(player_hand_value == 21):
-			return 0
-		elif(player_hand_value < 21):
+		if(is_ace_card == 1):
 			return -1
+		else:
+			if(player_hand_value == 21):
+				return 0
+			elif(player_hand_value < 21):
+				return -1
 	elif(dealer_hand_value < 21):
 		if(player_hand_value == 21):
 			return 1.5
@@ -63,4 +76,4 @@ def tellReward(player_hand_value, dealer_hand_value):
 for i in range(2,22):
 	print("\t")
 	for j in range(1,11):
-		print(str(i)+" "+str(j)+" "+str(rewardPlayer(i,j, probability_face_card)))
+		print(str(i)+" "+str(j)+" "+str(rewardPlayer(i,j, 0.37)))
