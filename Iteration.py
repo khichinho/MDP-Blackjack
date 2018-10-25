329e4ead521753c908b59108422dec57b3564a21
yash98 <yashmalviya98@gmail.com>
2018-10-25 22:30:52 +0530

    many bug fixes 1

--- Iteration.py
+++ Iteration.py
@@ -9,8 +9,17 @@ class Dp_solver:
         self.val_act_dict = {}
 
     def solve_state(self, rep_first, rep_second, dealer_fc, is_first):
-        if (rep_first, rep_second, dealer_fc, is_first) in self.val_act_dict:
-            return self.val_act_dict[(rep_first, rep_second, dealer_fc, is_first)][0]
+        # handle non splittable dup aces
+        if rep_first == rep_second and rep_first == 1 and not is_first:
+            return max([self.solve_state(0, 2, dealer_fc, False), self.solve_state(0, 12, dealer_fc, False)])
+
+        if rep_first != rep_second:
+            if (rep_first, rep_second, dealer_fc, is_first) in self.val_act_dict:
+                return self.val_act_dict[(rep_first, rep_second, dealer_fc, is_first)][0]
+        
+        if rep_first == 11:
+            if (rep_first, rep_second, dealer_fc, is_first) in self.val_act_dict:
+                return self.val_act_dict[(rep_first, rep_second, dealer_fc, is_first)][0]
         
         state_got = self.to_solve_mdp.states[(rep_first, rep_second, dealer_fc, is_first)]
         H_value = 0.0
@@ -24,7 +33,10 @@ class Dp_solver:
             elif trans.action == 'S':
                 S_value += trans.probability*self.solve_state(*trans.state_rep)
             elif trans.action == 'P':
-                P_value += trans.probability*self.solve_state(*trans.state_rep)
+                if rep_first == rep_second:
+                    P_value += trans.probability*self.val_act_dict[(rep_first, rep_second, dealer_fc, is_first)][0]
+                else:
+                    P_value += trans.probability*self.solve_state(*trans.state_rep)
             elif trans.action == 'D':
                 D_value += 2*trans.probability*self.solve_state(*trans.state_rep)
 
@@ -76,7 +88,7 @@ class Dp_solver:
                 self.val_act_dict[(dup, dup, dfc, True)] = (0.0, 'N')
                 prev_val = 0.0
                 curr_val = 1.0
-                epsilon = 0.01
+                epsilon = 0.1
                 # self.val_act_dict[(dup, dup, dfc, True)] = 
                 while not(-epsilon < (curr_val-prev_val) < epsilon):
                     prev_val = curr_val
@@ -137,6 +149,6 @@ class Dp_solver:
 
 if __name__ == "__main__":
     # mdp = Dp_solver(float(sys.argv[1]))
-    mdp = Dp_solver(4.0/13)
+    mdp = Dp_solver(0.1)
     mdp.solve_mdp()
     mdp.output_first_move_policy()
