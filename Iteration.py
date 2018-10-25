856cf34cd82535b5db8e575d4a70d3aa84f79074
yash98 <yashmalviya98@gmail.com>
2018-10-25 19:16:06 +0530

    Duplicate DP solving

--- Iteration.py
+++ Iteration.py
@@ -9,7 +9,7 @@ class Dp_solver:
 
     def solve_state(self, rep_first, rep_second, dealer_fc, is_first):
         if (rep_first, rep_second, dealer_fc, is_first) in self.val_act_dict:
-            return self.val_act_dict[(rep_first, rep_second, dealer_fc, is_first)]
+            return self.val_act_dict[(rep_first, rep_second, dealer_fc, is_first)][0]
         
         state_got = self.to_solve_mdp.states[(rep_first, rep_second, dealer_fc, is_first)]
         H_value = 0.0
@@ -28,18 +28,31 @@ class Dp_solver:
                 D_value += 2*trans.probability*self.solve_state(*trans.state_rep)
 
         max_value = max([H_value, S_value, P_value, D_value])
-        self.val_act_dict[(rep_first, rep_second, dealer_fc, is_first)] = max_value
+
+        if max_value == H_value:
+            best_action = 'H'
+        elif max_value == S_value:
+            best_action = 'S'
+        elif max_value == P_value:
+            best_action = 'P'
+        else:
+            best_action = 'D'
+        
+        # set in dict
+        self.val_act_dict[(rep_first, rep_second, dealer_fc, is_first)] = (max_value, best_action)
+        # return value
         return max_value
 
     def solve_mdp(self):
+        # 'G' is action stating its absorbing goal state 
         # Stand Goal states
         for player_hv in xrange(2, 22):
             for dfc in xrange(1, 11):
-                self.val_act_dict[(11, player_hv, dfc, False)] = Dealer.rewardPlayer(player_hv, dfc, self.fcp, False)
+                self.val_act_dict[(11, player_hv, dfc, False)] = (Dealer.rewardPlayer(player_hv, dfc, self.fcp, False), 'G')
 
         # Black Jack
         for dfc in xrange(1, 11):
-            self.val_act_dict[(11, 21, dfc, True)] = Dealer.rewardPlayer(21, dfc, self.fcp, True)
+            self.val_act_dict[(11, 21, dfc, True)] = (Dealer.rewardPlayer(21, dfc, self.fcp, True), 'G')
 
         # Bust
         self.val_act_dict[(21, 0, 0, False)] = -1.0
@@ -47,14 +60,23 @@ class Dp_solver:
         # first hard values
         for hard_value in xrange(2, 22):
             for dfc in xrange(1, 11):
-                self.val_act_dict[(0, hard_value, dfc, True)] = self.solve_state(0, hard_value, dfc, True)
+                # self.val_act_dict[(0, hard_value, dfc, True)] = 
+                self.solve_state(0, hard_value, dfc, True)
         
         # first soft values
         for soft_value in xrange(2, 10):
             for dfc in xrange(1, 11):
-                self.val_act_dict[(1, soft_value, dfc, True)] = self.solve_state(1, soft_value, dfc, True)
+                # self.val_act_dict[(1, soft_value, dfc, True)] = 
+                self.solve_state(1, soft_value, dfc, True)
         
         # duplicates
         for dup in xrange(1, 11):
             for dfc in xrange(1, 11):
-                val_act_dict[(dup, dup, dfc, True)] = State(dup, dup, dfc, True, False, face_card_probability)
+                self.val_act_dict[(dup, dup, dfc, True)] = 0.0
+                prev_val = 0.0
+                curr_val = 1.0
+                epsilon = 0.001
+                # self.val_act_dict[(dup, dup, dfc, True)] = 
+                while not(-epsilon < (curr_val-prev_val) < epsilon):
+                    prev_val = curr_val
+                    curr_val = self.solve_state(dup, dup, dfc, True)
