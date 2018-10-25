ab3d2dce73c1da605c84fd3f88e997769d1c06cf
Hardik Khichi <hardikkhichi@gmail.com>
2018-10-25 20:23:43 +0530

    Dealer finale

--- Dealer.py
+++ Dealer.py
@@ -13,59 +13,68 @@ reward_dict = {}
 
 def rewardPlayer(player_hand_value, dealer_first_card, probability_face_card, is_blackjack):
 	if(dealer_first_card == 1):
-		return(reward_player_calculate(player_hand_value, 1, 11, probability_face_card))
+		return(reward_player_calculate(player_hand_value, True, 11, probability_face_card, is_blackjack, 1))
 	else:
-		return(reward_player_calculate(player_hand_value, 0, dealer_first_card, probability_face_card))
+		return(reward_player_calculate(player_hand_value, False, dealer_first_card, probability_face_card, is_blackjack, 1))
 
-#is_ace_card == 1 (if ace is treated as 11)
-#else !1
-def reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value, probability_face_card):
+#is_ace_card == True (if ace is treated as 11)
+#else False
+def reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value, probability_face_card, is_blackjack, num_cards):
 
 	r = 0
-	config_key = (player_hand_value, is_ace_card, dealer_hand_value)
+	config_key = (player_hand_value, is_ace_card, dealer_hand_value, is_blackjack, num_cards)
 
 	if config_key in reward_dict:
 		return reward_dict[config_key]
 	else:
 		if(dealer_hand_value > 21):
-			if(is_ace_card == 1):
-				return(reward_player_calculate(player_hand_value,0,dealer_hand_value-10, probability_face_card))
+			if(is_ace_card == True):
+				return(reward_player_calculate(player_hand_value,False,dealer_hand_value-10, probability_face_card, is_blackjack, num_cards))
 			else:
-				r = tellReward(player_hand_value, is_ace_card, dealer_hand_value)
+				r = tellReward(player_hand_value, is_ace_card, dealer_hand_value, is_blackjack, num_cards)
 				reward_dict[config_key] = r
 		elif(dealer_hand_value >= 17):
-			r = tellReward(player_hand_value, is_ace_card, dealer_hand_value)
+			r = tellReward(player_hand_value, is_ace_card, dealer_hand_value, is_blackjack, num_cards)
 			reward_dict[config_key] = r
 		else:
 			for i in range(1,11):
 				if(i == 10):
-					r += probability_face_card * reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value+10, probability_face_card)
+					r += probability_face_card * reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value+10, probability_face_card, is_blackjack, num_cards+1)
 					reward_dict[config_key] = r
 				elif(i == 1):
-					r += ((1-probability_face_card)/9) * reward_player_calculate(player_hand_value, 1, dealer_hand_value+11, probability_face_card)
+					r += ((1-probability_face_card)/9) * reward_player_calculate(player_hand_value, True, dealer_hand_value+11, probability_face_card, is_blackjack, num_cards+1)
 					reward_dict[config_key] = r
 				else:
-					r += ((1-probability_face_card)/9) * reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value+i, probability_face_card)
+					r += ((1-probability_face_card)/9) * reward_player_calculate(player_hand_value, is_ace_card, dealer_hand_value+i, probability_face_card, is_blackjack, num_cards+1)
 					reward_dict[config_key] = r
 		return(r)
 
-def tellReward(player_hand_value, is_ace_card, dealer_hand_value):
+def tellReward(player_hand_value, is_ace_card, dealer_hand_value, is_blackjack, num_cards):
 	if(dealer_hand_value > 21):
-		if(player_hand_value == 21):
+		if(is_blackjack == True):
 			return 1.5
-		elif(player_hand_value < 21):
+		else:
 			return 1
 	elif(dealer_hand_value == 21):
-		if(is_ace_card == 1):
-			return -1
+		if(player_hand_value == 21):
+			if(is_blackjack == True):
+				if(is_ace_card == True and num_cards == 2):
+					return 0
+				else:
+					return 1.5
+			elif(is_blackjack == False):
+				if(is_ace_card == True and num_cards == 2):
+					return -1
+				else:
+					return 0
 		else:
-			if(player_hand_value == 21):
-				return 0
-			elif(player_hand_value < 21):
-				return -1
+			return -1
 	elif(dealer_hand_value < 21):
 		if(player_hand_value == 21):
-			return 1.5
+			if(is_blackjack == True):
+				return 1.5
+			else:
+				return 1
 		elif(player_hand_value > dealer_hand_value):
 			return 1
 		elif(player_hand_value == dealer_hand_value):
@@ -77,4 +86,8 @@ if __name__ == "__main__":
 	for i in range(2,22):
 		print("\t")
 		for j in range(1,11):
-			print(str(i)+" "+str(j)+" "+str(rewardPlayer(i,j, 0.37)))
+			print(str(i)+" "+str(j)+" "+str(rewardPlayer(i,j, 0.37, False)))
+	
+	print("\t")
+	for j in range(1,11):
+			print(str(21)+" "+str(j)+" "+str(rewardPlayer(21,j, 0.37, True)))
---
+++ dp.py
@@ -0,0 +1,6 @@
+import Dealer
+import Markov
+
+p = 0.37
+
+blackjack = Markov(0.37)
